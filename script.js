2ca2cdac38b6d446cc3afcc55a0c018e9ab6863c
3oF <tuukka.peuralahti@gmail.com>
2019-10-17 18:54:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 // READ THE WIKI BEFORE USING THIS CODE
 // Seriusly You Wont Understand this
-
+// maybe but newbies before editing on your own fork on github or on our handy dandy Text Editor/Ide
 
 
 function main(){
   
-  //var canvasWidht = 600;
-  //var canvasHeight = 600;
+  var canvasWidth = 600;
+  var canvasHeigth = 600;
   
-  // Specification For The Canvas (required html canvas called canvas!
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+  var canvas = document.getElemntById("canvas");
+  canvas.width = canvasWidth;
+  canvas.heigth = canvasHeigth;
+  context = canvas.getContext("2d");
   
   // Define The "Ball" Image Object
   var ball = new Image();
   // Get Image From This Repo
   ball.src = 'https://tpelaaja.github.io/BallHole/ball.png;
-  
   // Starting Positions. Change Them How ever you like them!
   var ballX = 0
   var ballY = 0;
   // Info About The Balls Size (64x64) Read About Changing The Ball On The Wiki!
   var ballWidth = 64;
   var ballHeight = 64;
+  
+  var Background = new Image();
+  Background.src="https://tpelaaja.github.io/BallHole/randombg.png";
+  context.drawImage(Background,0,0);
+  
+  function draw(){
+    drawBackGround();
+    drawBall();
+  }
+  
+  function drawBackGround(){
+    context.drawImage(Background,0,0);
+  };
   
   // This Function Draws The Ball. Read About The Changing Ball On The Wiki!
   function drawBall();
