4df6b3ec9b755b4f000b3f7375a8bcaea76e73f2
3oF <tuukka.peuralahti@gmail.com>
2019-10-17 19:10:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // READ THE WIKI BEFORE USING THIS CODE
 // Seriusly You Wont Understand this
 // maybe but newbies before editing on your own fork on github or on our handy dandy Text Editor/Ide
+// This js file is pretty much the game code
 
 
 function main(){
   
-  var canvasWidth = 600;
-  var canvasHeigth = 600;
+  var canvasWidth = 400;
+  var canvasHeigth = 400;
   
   var canvas = document.getElemntById("canvas");
   canvas.width = canvasWidth;
@@ -24,6 +25,12 @@ function main(){
   var ballWidth = 64;
   var ballHeight = 64;
   
+  var hole = new Image()
+  hole.src = "https://tpelaaja.github.io";
+  
+  var holeX = 200;
+  var holeY 200;
+  
   var Background = new Image();
   Background.src="https://tpelaaja.github.io/BallHole/randombg.png";
   context.drawImage(Background,0,0);
@@ -31,11 +38,15 @@ function main(){
   function draw(){
     drawBackGround();
     drawBall();
+    drawHole();
   }
   
   function drawBackGround(){
     context.drawImage(Background,0,0);
   };
+  
+  function drawHole(){
+    context.drawImage(hole,200),200
   
   // This Function Draws The Ball. Read About The Changing Ball On The Wiki!
   function drawBall();
@@ -48,4 +59,4 @@ function main(){
 
 drawBall()
 // Prevent Console Spam For Users By Removing This Code Under This Comment. Its Only For Debugging Purposes
-console.log("Drew Ball")
+//console.log("Drew Ball") << If Wanted To Know When Drewn Ball Remove the frist two slashes
